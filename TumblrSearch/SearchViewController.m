492c916acead8936b24d6e8daa2f78280a5f6748
Mirczenzo <mateusz.mirkowski@visentcoders.com>
2016-12-29 10:34:01 +0100

    Remove storyboard and load initial view controller programmatically

--- TumblrSearch/SearchViewController.m
+++ TumblrSearch/SearchViewController.m
@@ -6,23 +6,22 @@
 //  Copyright © 2016 Mateusz Mirkowski. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "SearchViewController.h"
 
-@interface ViewController ()
+@interface SearchViewController ()
 
 @end
 
-@implementation ViewController
+@implementation SearchViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  NSLog(@"search view controller");
 }
 
-
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
 }
 
 
